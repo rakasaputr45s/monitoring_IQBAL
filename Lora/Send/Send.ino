0a997be805104e3d6f6d28a37c1e5f8b2f5564e0
rakasaputr45s <rakasaputrcdc@gmail.com>
2023-05-20 00:48:20 +0700

    add LorA recieve

--- Lora/Send/Send.ino
+++ Lora/Send/Send.ino
@@ -8,28 +8,12 @@ LoRa_E32 e32ttl100(&Serial1, 18, 21, 19); //  RX AUX M0 M1
 void setup() {
 Serial.begin(9600);
 delay(500);
-Serial.println("Hi, I'm going to send message!");
+Serial.println("SHUT UP LORa");
 // Startup all pins and UART
 e32ttl100.begin();
-// Send message
-ResponseStatus rs = e32ttl100.sendMessage("HALLO_DOG");
-// Check If there is some problem of succesfully send
-Serial.println(rs.getResponseDescription());
 }
 void loop() 
 {
-//if (e32ttl100.available()>1) {
-//// read the String message
-//ResponseContainer rc = e32ttl100.receiveMessage();
-//// Is something goes wrong print error
-//if (rc.status.code!=1)   rc.status.getResponseDescription();
-//else                     Serial.println(rc.data);
-//}
-//
-//if (Serial.available()) {
-//String input = Serial.readString();
-//e32ttl100.sendMessage(input);
-//}
 ResponseStatus rs = e32ttl100.sendMessage("HALLO_DEC");
 // Check If there is some problem of succesfully send
 Serial.println(rs.getResponseDescription());
